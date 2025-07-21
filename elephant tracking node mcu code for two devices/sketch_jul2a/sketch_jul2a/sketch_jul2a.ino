2faefe0634ce3aeefcd756142f957278afd3a1fd
Shan indeewa <139702069+ShanIndeewa@users.noreply.github.com>
2025-07-21 17:23:31 +0530

    Add onother divice code.

---
+++ elephant tracking node mcu code for two devices/sketch_jul2a/sketch_jul2a/sketch_jul2a.ino
@@ -0,0 +1,95 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266Firebase.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+
+// === WiFi & Firebase Config ===
+#define WIFI_SSID "Suraj"
+#define WIFI_PASSWORD "11114444"
+#define FIREBASE_URL "https://elephant-tracker-44677-default-rtdb.firebaseio.com/"
+Firebase firebase(FIREBASE_URL);
+
+// The pins you connected the GPS TX/RX to
+static const int RXPin = 4;  // NodeMCU D2 (GPIO4)
+static const int TXPin = 5;  // NodeMCU D1 (GPIO5)
+
+// === Device ID ===
+#define DEVICE_ID "E02"
+
+// -- GPS Baud Rate --
+static const uint32_t GPSBaud = 9600;
+
+TinyGPSPlus gps;
+
+// Create a SoftwareSerial object to communicate with the GPS
+SoftwareSerial ss(RXPin, TXPin);
+
+// === NTP Client Setup ===
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 19800, 60000); // UTC+5:30
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+
+  // Start the software serial for the GPS
+  ss.begin(GPSBaud);
+  Serial.println("Booting...");
+
+  Serial.println("NodeMCU NEO-M8N GPS Test");
+  Serial.println("Searching for GPS satellites...");
+  
+  timeClient.begin();
+  timeClient.update();
+
+  firebase.setString("/elephants/" + String(DEVICE_ID) + "/id", DEVICE_ID);
+  Serial.println("Firebase initialized");
+}
+
+void loop() {
+
+   while (ss.available() > 0) {
+    gps.encode(ss.read());
+  }
+
+    // If no GPS data is received for 5 seconds, print a message
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+    Serial.println("No GPS data detected: check wiring or antenna placement.");
+    while(true); // Halt the program63
+  }
+  
+
+  if (gps.location.isValid()) {
+    float lat = gps.location.lat();
+    float lng = gps.location.lng();
+
+    timeClient.update();
+    String formattedTime = timeClient.getFormattedTime();
+    unsigned long epoch = timeClient.getEpochTime();
+
+    String basePath = "/elephants/" + String(DEVICE_ID) + "/position";
+
+    firebase.setFloat(basePath + "/lat", lat);
+    firebase.setFloat(basePath + "/lng", lng);
+    firebase.setString(basePath + "/time", formattedTime);
+    firebase.setInt(basePath + "/timestamp", epoch);
+
+    Serial.println("Lat: " + String(lat, 6));
+    Serial.println("Lng: " + String(lng, 6));
+    Serial.println("Time: " + formattedTime);
+    Serial.println("------------------------");
+
+  } else {
+    Serial.println("Waiting for GPS fix...");
+
+  }
+}
